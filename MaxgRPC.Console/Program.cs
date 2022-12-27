9d8ad1821cff44da5dceb7840459031eafc4cfad
Kunlapath <kunlapath.paengsa@gmail.com>
2022-12-27 12:30:30 +0700

    Add project files.

---
+++ MaxgRPC.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using MaxgRPC;
+
+var inputName = new HelloRequest { Name= "max" };
+
+var channel = GrpcChannel.ForAddress("https://localhost:7107/");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(inputName);
+
+Console.WriteLine(reply.Message);
+Console.ReadLine();
